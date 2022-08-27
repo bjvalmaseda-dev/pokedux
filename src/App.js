2bc1e0ebb7e2e22444900c2670ddb31f8b2209dd
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-27 17:13:33 -0400

    :sparkles: [ADD] API integration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import "./App.css";
+import React, { useEffect, useState } from "react";
 import { Col } from "antd";
 import Searcher from "./components/Searcher";
 import PokemonList from "./components/PokemonList";
+import { getPokemons } from "./api";
+import Logo from "./statics/logo.svg";
+import "./App.css";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const pokemonsRes = await getPokemons();
+      setPokemons(pokemonsRes);
+    };
+    fetchPokemon();
+  }, []);
+
   return (
     <div className="App">
+      <Col span={4} offset={10}>
+        <img src={Logo} alt="Pokedux" />
+      </Col>
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList />
+      <PokemonList pokemons={pokemons} />
     </div>
   );
 }
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -6,7 +6,7 @@ const PokemonList = ({ pokemons }) => {
   return (
     <div className="PokemonList">
       {pokemons.map((pokemon) => (
-        <PokemonCard />
+        <PokemonCard name={pokemon.name} key={pokemon.name} />
       ))}
     </div>
   );
