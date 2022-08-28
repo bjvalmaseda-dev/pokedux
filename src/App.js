d525d6413300d3dffb39e2a0c2886a71df1c556c
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-28 08:15:15 -0400

    :sparkles: [ADD] spinner for loading state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { useEffect } from "react";
 import { Col } from "antd";
+import { Spin } from "antd";
 import Searcher from "./components/Searcher";
 import PokemonList from "./components/PokemonList";
 import { getPokemons } from "./api";
 import Logo from "./statics/logo.svg";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
-import { getPokemonWithDetails } from "./actions";
+import { getPokemonWithDetails, setLoading } from "./actions";
 
 function App() {
   const pokemons = useSelector((state) => state.pokemons);
+  const loading = useSelector((state) => state.loading);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchPokemon = async () => {
+      dispatch(setLoading(true));
       const pokemonsRes = await getPokemons();
       dispatch(getPokemonWithDetails(pokemonsRes));
+      dispatch(setLoading(false));
     };
     fetchPokemon();
   }, []);
@@ -28,7 +32,13 @@ function App() {
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
-      <PokemonList pokemons={pokemons} />
+      {loading ? (
+        <Col offset={12}>
+          <Spin spinning size="large" />
+        </Col>
+      ) : (
+        <PokemonList pokemons={pokemons} />
+      )}
     </div>
   );
 }
--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Input } from "antd";
 
 const Searcher = () => {
-  return <Input.Search placeholder="buscar..." />;
+  return <Input.Search placeholder="buscar..." style={{ marginBottom: 10 }} />;
 };
 
 export default Searcher;
