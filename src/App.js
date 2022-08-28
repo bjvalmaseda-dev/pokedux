8e79f95b0ab5b731c115d262047f03f69ab5e215
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-08-27 21:19:05 -0400

    :recycle: [ADD] redux integrations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import { Col } from "antd";
 import Searcher from "./components/Searcher";
 import PokemonList from "./components/PokemonList";
 import { getPokemons } from "./api";
+import { setPokemons as setPokemonsAction } from "./actions";
 import Logo from "./statics/logo.svg";
 import "./App.css";
 
-function App() {
-  const [pokemons, setPokemons] = useState([]);
-
+function App({ pokemons, setPokemons }) {
+  console.log(pokemons);
   useEffect(() => {
     const fetchPokemon = async () => {
       const pokemonsRes = await getPokemons();
@@ -30,4 +31,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  pokemons: state.pokemons,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setPokemons: (value) => dispatch(setPokemonsAction(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
